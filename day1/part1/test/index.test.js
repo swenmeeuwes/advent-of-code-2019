f289096293565b2e03f34f46979a28aa1429207a
Swen Meeuwes <swen@ranj.nl>
2019-12-08 01:33:49 +0100

    Update test description

--- day1/part1/test/index.test.js
+++ day1/part1/test/index.test.js
@@ -7,7 +7,7 @@ describe('Fuel computation from component masses', function() {
       assert.equal(index.computeFuelNeeded([12, 14]), 4);
     });
 
-    it('should correctly handle negative fuel values', function() {
+    it('should correctly handle light weight components', function() {
       assert.equal(index.computeFuelNeeded([3, 1, 2, 5]), 0);
     });
   });
